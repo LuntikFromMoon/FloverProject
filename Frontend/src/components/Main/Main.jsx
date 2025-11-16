129ccfab806d1dbcdfdfe1fcea8b55637d28ade9
LuntikFromMoon <fantomblue25@gmail.com>
2025-11-16 15:50:12 +0300

    Создала сааамую основу каталога

--- Frontend/src/components/Main/Main.jsx
+++ Frontend/src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import styles from "./Main.module.css";
 import MainTopPart from './MainTopPart/MainTopPart.jsx';
 import MainBotPart from "./MainBotPart/MainBotPart.jsx";
+import MainMiddlePart from "./MainMiddlePart/MainMiddlePart.jsx";
 
 export const Main = () => {
     return ( <div className={styles.main}>
         <MainTopPart/>
+        <MainMiddlePart/>
         <MainBotPart/>
     </div>)
 };
