ac5f343d5acdb98da385a40aaf73152a6e25f680
LuntikFromMoon <fantomblue25@gmail.com>
2025-11-16 21:24:23 +0300

    feat: Сделала шаблон для карусельки

---
+++ Frontend/src/components/Main/MainMiddlePart/MainMiddlePart.jsx
@@ -0,0 +1,21 @@
+import styles from './MainMiddlePart.module.css';
+import catalog_photo from "../../../assets/pictures/roses.jpg";
+import CatItem from "./CatalogItem/CatalogItem.jsx";
+
+const MainMiddlePart = () => {
+    return <div className={styles.main__middle}>
+        <div className={styles.main__title}>
+            <p className={styles.main__text}>Каталог</p>
+        </div>
+        <div className={styles.main__catalog}>
+            <CatItem image={catalog_photo} title={"Алые Розы"} price={4000}></CatItem>
+            <CatItem image={catalog_photo} title={"Алые Розы"} price={4000}></CatItem>
+            <CatItem image={catalog_photo} title={"Алые Розы"} price={4000}></CatItem>
+            <CatItem image={catalog_photo} title={"Алые Розы"} price={4000}></CatItem>
+            <CatItem image={catalog_photo} title={"Алые Розы"} price={4000}></CatItem>
+            <CatItem image={catalog_photo} title={"Алые Розы"} price={4000}></CatItem>
+        </div>
+    </div>
+};
+
+export default MainMiddlePart;
